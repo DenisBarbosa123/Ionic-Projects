149708426c08aaa4878fb024165032c65d0f838d
Denis Barbosa <denis_barbosa123@hotmail.com>
2019-07-25 21:13:23 -0300

    add page de carregando e de atualizar e recarregar pagina de feed

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, LoadingController } from 'ionic-angular';
 import { checkNoChangesView } from '@angular/core/src/view/view';
 import {MoovieProvider} from '../../providers/moovie/moovie'
+import { ThrowStmt } from '@angular/compiler';
 /**
  * Generated class for the FeedPage page.
  *
@@ -27,16 +28,42 @@ export class FeedPage {
   }
   public lista_filmes = new Array<any>();
   public nome_usuario:string = 'Denis Barbosa';//variavel do tipo string
-
+  public loader;
+  public refresher;
+  public isRefreshing:boolean = false;
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    private movieProvider: MoovieProvider ) {
+    private movieProvider: MoovieProvider,
+    public loadingCtrl: LoadingController ) {
   }
 
+  openLoading() { //abre tela de carregando
+    //console.log('carregando...');
+      this.loader = this.loadingCtrl.create({
+      content: "Please wait...",
+    });
+    this.loader.present();
+  }
 
+  closeLoading(){ //fecha tela de carregando
+    //console.log('terminou de carregar...');
+    this.loader.dismiss();
+  }
 
-  ionViewDidLoad() {//metodo que carrega junto com tela
+  doRefresh(refresher) {//metodo que atualiza lista de filmes
+    this.refresher = refresher;
+    this.isRefreshing=true;
+    this.carregarFilmes();
+  }
+
+  ionViewDidEnter() {//metodo que carrega junto com tela
+    this.carregarFilmes();
+  }
+
+  carregarFilmes(){
+    this.openLoading();
+    //console.log('abrindo filmes...');
     this.movieProvider.getLetesMovies().subscribe(
       data=>{
 
@@ -44,9 +71,20 @@ export class FeedPage {
         const objeto_retorno = JSON.parse(response._body);//faz parse de json para objeto
         this.lista_filmes = objeto_retorno.results;
         
-        console.log(objeto_retorno); 
+        //console.log(objeto_retorno); 
+        this.closeLoading();
+        if(this.isRefreshing){
+          this.refresher.complete();
+          this.isRefreshing=false;
+        }
       }, error=>{
         console.log(error);
+        this.closeLoading();
+        if(this.isRefreshing){
+          this.refresher.complete();
+          this.isRefreshing=false;
+
+        }
       }
     )
   }
